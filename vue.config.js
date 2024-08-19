35c27ad64af981672f0262c9d098ad956b4ade06
CXNOT0649 <CXNOT0649@token.ad>
2024-08-19 12:21:13 +0800

    初始化

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath:process.env.NODE_NEV==='production' ? '': '/',
+  outputDir:process.env.NODE_NEV==='production' ? 'dist': 'devDist',//打包称那个文件夹
   transpileDependencies: true,
+  lintOnSave:false
 });
