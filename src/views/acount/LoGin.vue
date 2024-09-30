e4f5a57b62f9050ade3db4d927f590ede8b47c1d
CXNOT0649 <CXNOT0649@token.ad>
2024-09-30 10:33:12 +0800

    跨域

--- src/views/acount/LoGin.vue
+++ src/views/acount/LoGin.vue
@@ -1,73 +1,87 @@
-<template>
-    <a-form name="custom-validation" ref="formRef" :model="formState" :rules="rules" v-bind="layout" @finish="handleFinish"
-        @finishFailed="handleFinishFailed">
-        <a-form-item has-feedback label="Password" name="pass">
-            <a-input type="password" autocomplete="off" />
-        </a-form-item>
-        <a-form-item has-feedback label="Confirm" name="checkPass">
-            <a-input type="password" autocomplete="off" />
-        </a-form-item>
-        <a-form-item :wrapper-col="{ span: 14, offset: 4 }">
-            <a-button type="primary" html-type="submit">Submit</a-button>
-            <a-button style="margin-left: 10px" @click="resetForm">Reset</a-button>
-        </a-form-item>
-    </a-form>
+<template >
+    <div class="hearder-bgd">
+        <a-form name="custom-validation" ref="formRef" :model="formState" :rules="rules">
+            <a-form-item has-feedback label="" :name="rules.usernname" :wrapper-col="{ span: 4, offset: 10 }">
+                <label class="label-bgd-w"> username</label>
+                <a-input type="text" autocomplete="off" class="input-mb-10" />
+            </a-form-item>
+            <a-form-item has-feedback label="" :name="rules.password" :wrapper-col="{ span: 4, offset: 10 }">
+                <label class="label-bgd-w"> password</label>
+                <a-input type="password" autocomplete="off" class="input-mb-10" />
+            </a-form-item>
+            <a-form-item :wrapper-col="{ span: 14, offset: 10 }">
+                <a-button type="primary" html-type="submit" @click="submit">Submit</a-button>
+                <a-button style="margin-left: 10px">Reset</a-button>
+            </a-form-item>
+        </a-form>
+    </div>
 </template>
 
 <script>
 import { reactive } from 'vue';
+import { getUser } from "@/api/user.js"
 export default {
     setup() {
-        let validatePass = async (rule, value) => {
-            if (value === '') {
-                return Promise.reject('Please input the password');
-            } else {
-                if (formState.checkPass !== '') {
-                    formRef.value.validateFields('checkPass');
-                }
-                return Promise.resolve();
+        const rules = {
+            usernname:
+            {
+                required: true,
+                message: 'Please input name',
+            },
+            password:
+            {
+                required: true,
+                message: 'Please input password',
             }
-        };
-        let validatePass2 = async (rule, value) => {
-            if (value === '') {
-                return Promise.reject('Please input the password again');
-            } else if (value !== formState.pass) {
-                return Promise.reject("Two inputs don't match!");
-            } else {
-                return Promise.resolve();
-            }
-        };
+
+        }
         const data = reactive({
+            formState: {
+                username: '',
+                password: ''
+            },
+
             layout: {
                 labelCol: {
                     span: 4,
                 },
                 wrapperCol: {
-                    span: 14,
+                    span: 40,
                 }
             },
-            rules: {
-                pass: [
-                    {
-                        required: true,
-                        validator: validatePass,
-                        trigger: 'change',
-                    },
-                ],
-                checkPass: [
-                    {
-                        validator: validatePass2,
-                        trigger: 'change',
-                    },
-                ],
-
-            }
         })
-        
-        return data
+        const submit = () => {
+            getUser({data:'',message:'邮箱',resCode:'1003'})
+
+        }
+
+        return { ...data, rules, submit }
 
     }
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.hearder-bgd {
+    width: 100%;
+    height: 100%;
+    background: black;
+    opacity: .8;
+}
+
+.ant-form {
+    padding-top: 300px;
+}
+
+.ant-form-item {
+    margin-bottom: 36px;
+}
+
+.input-mb-10 {
+    margin-top: 10px;
+}
+
+.label-bgd-w {
+    color: #ffffff;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ const resolve = dir => {
 }
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath:process.env.NODE_NEV==='production' ? '': '/',
+  publicPath:process.env.NODE_NEV==='production' ? '': './',
   outputDir:process.env.NODE_NEV==='production' ? 'dist': 'devDist',//打包称那个文件夹
   transpileDependencies: true,
   lintOnSave:false,
@@ -31,7 +31,21 @@ module.exports = defineConfig({
         symbolId: 'icon-[name]'
       })
       .end()
+  },
+  devServer: {
+    open:false,
+    host:"0.0.0.0",//外部访问
+    port:8000,
+    proxy: {
+      [process.env.VUE_APP_FLAG]: {
+        target:process.env.VUE_APP_APIURL, // 目标API地址
+        ws:false,
+        changeOrigin: true, // 开启代理，在本地创建一个虚拟服务器，然后发送请求的数据，会同时会收到请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
+        pathRewrite: {
+          [`^${process.env.VUE_APP_FLAG}`]: '' // 路径重写，移除路径中的/api
+        }
+      }
+    }
   }
-
 
 });
